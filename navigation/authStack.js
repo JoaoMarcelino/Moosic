f63fb339fcbf1a4a6ff4c8464e28ff12092b5b0f
Gonçalo São Marcos <goncalomarcos@hotmail.com>
2020-11-24 14:55:10 +0000

    braaap

--- navigation/authStack.js
+++ navigation/authStack.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import LoginScreen from "../screens_auth/login";
 import SignupScreen from "../screens_auth/register";
@@ -9,45 +8,23 @@ import AuthScreen from "../screens_auth/authentication";
 const Stack = createStackNavigator();
 
 const AuthStack = () => {
-  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
-  let routeName;
-
-  useEffect(() => {
-    AsyncStorage.getItem("alreadyLaunched").then((value) => {
-      if (value == null) {
-        AsyncStorage.setItem("alreadyLaunched", "true");
-        setIsFirstLaunch(true);
-      } else {
-        setIsFirstLaunch(false);
-      }
-    });
-  }, []);
-
-  if (isFirstLaunch == null) {
-    return null;
-  } else if (isFirstLaunch == true) {
-    routeName = "Onboarding";
-  } else {
-    routename = "Start";
-  }
-
   return (
-    <Stack.Navigator> 
-        <Stack.Screen
+    <Stack.Navigator initialRouteName={"Authentication"}>
+      <Stack.Screen
         name="Authentication"
         component={AuthScreen}
-        options={{ title: 'Welcome' }}
-        />
-        <Stack.Screen
+        options={{ title: "Welcome" }}
+      />
+      <Stack.Screen
         name="Login"
         component={LoginScreen}
-        options={{ title: 'Login' }}
-        />
-        <Stack.Screen
+        options={{ title: "Login" }}
+      />
+      <Stack.Screen
         name="Register"
         component={SignupScreen}
-        options={{ title: 'Register' }}
-        />
+        options={{ title: "Register" }}
+      />
     </Stack.Navigator>
   );
 };
