5c127b50db5196a06fcdadf22d38e1694efc7b5f
Josphze <43673761+Josphze@users.noreply.github.com>
2020-12-08 18:52:24 +0000

    Cool bar

--- navigation/appBottomTab.js
+++ navigation/appBottomTab.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import HomeView from "../screens_home/homeView";
 import Search from "../screens_home/search";
 import Vault from "../screens_home/vault";
@@ -7,31 +7,39 @@ import Profile from "../screens_home/profile";
 import AddMusic from "../screens_home/addMusic";
 import AddAlbum from "../screens_home/addAlbum";
 import VaultStack from "../navigation/vaultStack";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 const AppBottomTab = (context) => {
-  return (
-    <Tab.Navigator
-      activeColor="#55D9C1"
-      inactiveColor="#989898"
-      shifting={false}
-      barStyle={{ backgroundColor: "#151515", height: 50 }}
-    >
-      <Tab.Screen name="Home" component={HomeView} />
-      <Tab.Screen
-        name="Search"
-        component={Search}
-        initialParams={context.route.params}
-      />
-      <Tab.Screen
-        name="Vault"
-        component={VaultStack}
-        initialParams={context.route.params}
-      />
-      <Tab.Screen name="Profile" component={Profile} />
-    </Tab.Navigator>
-  );
+	return (
+		<Tab.Navigator
+			tabBarPosition="bottom"
+			initialRouteName="Home"
+			shifting={false}
+			tabBarOptions={{
+				indicatorStyle: { backgroundColor: "#55D9C1" },
+				activeTintColor: "#55D9C1",
+				inactiveTintColor: "#989898",
+				labelStyle: { fontSize: 12 },
+				tabStyle: { height: 50 },
+				style: { backgroundColor: "#151515" },
+			}}
+		>
+			<Tab.Screen name="Home" component={HomeView} />
+			<Tab.Screen
+				name="Search"
+				component={Search}
+				initialParams={context.route.params}
+			/>
+			<Tab.Screen
+				name="Vault"
+				component={VaultStack}
+				initialParams={context.route.params}
+			/>
+			<Tab.Screen name="Profile" component={Profile} />
+		</Tab.Navigator>
+	);
 };
 
 export default AppBottomTab;
