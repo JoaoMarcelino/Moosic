abcb04a8f0ece781b99b7d10b8a0b723f2b6219b
João Teixeira <44053859+jteixeira00@users.noreply.github.com>
2020-12-10 18:33:09 +0000

    fixed albums

--- screens_home/myAlbums.js
+++ screens_home/myAlbums.js
@@ -14,8 +14,49 @@ class MyAlbums extends React.Component {
     super(props);
     this.state = { ...initial_state };
     this.context = this.props.route.params.context;
+    this.willFocusListener = this.props.navigation.addListener("focus", () => {
+      this.componentWillFocus();
+    });
   }
 
+  componentWillFocus() {
+    this.updateMusic2();
+    console.log("focus");
+  }
+  updateMusic2() {
+    let albumList = [];
+    let object = {};
+    this.setState({ loading: true });
+    console.log(this.props.route.params.context.albums());
+    this.props.route.params.context.albums().then((querySnapshot) => {
+      querySnapshot.forEach(function (doc) {
+        object = doc.data();
+        object.id = doc.id;
+        albumList.push(object);
+      });
+      this.setState({ albumList });
+      this.setState({ loading: false });
+    });
+  }
+
+  updateMusic(item) {
+    this.props.route.params.context.removeAlbum(item);
+    let albumList = [];
+    let object = {};
+    this.setState({ loading: true });
+    console.log(this.props.route.params.context.albums());
+    this.props.route.params.context.albums().then((querySnapshot) => {
+      querySnapshot.forEach(function (doc) {
+        object = doc.data();
+        object.id = doc.id;
+        if (object.id != item.id) {
+          albumList.push(object);
+        }
+      });
+      this.setState({ albumList });
+      this.setState({ loading: false });
+    });
+  }
   componentDidMount() {
     let albumList = [];
     let object = {};
@@ -39,9 +80,7 @@ class MyAlbums extends React.Component {
         <StatusBar backgroundColor="black" barStyle="light-content" />
         <Text>My Albums</Text>
         <FormButton
-          onPress={() =>
-            this.props.navigation.navigate("AddAlbum")
-          }
+          onPress={() => this.props.navigation.navigate("AddAlbum")}
         />
         <FlatList
           data={albumList}
@@ -52,9 +91,9 @@ class MyAlbums extends React.Component {
               </Text>
               <View style={styles.form}>
                 <FormButton
-                  onPress={() =>
-                    this.props.route.params.context.removeAlbum(item)
-                  }
+                  onPress={() => {
+                    this.updateMusic(item);
+                  }}
                   buttonTitle="Remove"
                 />
               </View>
