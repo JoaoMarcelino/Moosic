53f8f3c6d333a93a60931fd5826c56034a299067
Gonçalo São Marcos <goncalomarcos@hotmail.com>
2020-12-10 17:23:08 +0000

    Ja te podes divertir com firebase nos Changes @jteixeira00
    🐮🐮🐮🐮🐮

--- screens_home/changeProfileName.js
+++ screens_home/changeProfileName.js
@@ -1,5 +1,16 @@
 import React from "react";
-import { StatusBar, StyleSheet, Text, View, SectionList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FormButton from "../components/FormButton";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import {
+  ImageBackground,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 const initialState = {};
 class ChangeProfileName extends React.Component {
@@ -10,20 +21,131 @@ class ChangeProfileName extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Change Profile Name</Text>
-      </View>
+      <SafeAreaView style={styles.safeView}>
+        <StatusBar backgroundColor="black" barStyle="light-content" />
+        <ImageBackground
+          source={require("../assets/400x800.png")}
+          style={styles.backgroundImage}
+        >
+          <View style={styles.container}>
+            <View style={styles.pageHeader}>
+              <TouchableOpacity
+                style={styles.crossmark}
+                onPress={() => {
+                  this.props.navigation.navigate("Settings");
+                }}
+              >
+                <FontAwesome name={"times"} size={24} color={"#0D0D0D"} />
+              </TouchableOpacity>
+              <Text style={styles.pageHeaderText}>Change Profile Name</Text>
+            </View>
+            <View style={styles.form}>
+              <TextInput
+                style={styles.inputForm}
+                placeholder="New Profile Name"
+                placeholderStyle={styles.inputFormText}
+                placeholderTextColor="#0D0D0D"
+                // CODIGO DE ACTUALLY MUDAR O NOME AQUI PROVAVELMENTE  @JOAO TEIXEIRA
+              />
+              <TextInput
+                style={styles.inputForm}
+                placeholder="Confirm New Profile Name"
+                placeholderStyle={styles.inputFormText}
+                placeholderTextColor="#0D0D0D"
+                //  DEVE CHECKAR SE SAO IGUAIS I GUESS, SO DEIXA MUDAR SE FOREM IGUAIS SENAO MANDA UM alert() OU ALGO DO GENERO @JOAO TEIXEIRA
+              />
+              <FormButton onPress={this.onSubmit} buttonTitle="Save Changes" />
+            </View>
+          </View>
+          <View style={{ flex: 2 }} />
+        </ImageBackground>
+      </SafeAreaView>
     );
   }
 }
 
 export default ChangeProfileName;
 
+//No font on placeholders cause of a react bug
 const styles = StyleSheet.create({
-  container: {
+  backgroundImage: {
     flex: 1,
-    backgroundColor: "#fff",
+    resizeMode: "cover",
+  },
+
+  container: {
+    flex: 3,
     alignItems: "center",
+    padding: 36,
+  },
+
+  checkboxContainer: {
+    backgroundColor: "#F2F2F2",
+    borderColor: "#0D0D0D",
+    borderStyle: "solid",
+    borderWidth: 3,
+    borderRadius: 100,
+    height: 40,
     justifyContent: "center",
+  },
+
+  checkboxContainerText: {
+    fontFamily: "Inter Regular",
+    fontStyle: "normal",
+    fontWeight: "600",
+    fontSize: 14,
+    lineHeight: 17,
+    color: "#0D0D0D",
+  },
+
+  crossmark: {
+    position: "absolute",
+    left: 16,
+  },
+
+  form: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+
+  inputForm: {
+    backgroundColor: "#F2F2F2",
+    borderRadius: 8,
+    borderColor: "#0D0D0D",
+    borderStyle: "solid",
+    borderWidth: 3,
+    height: 57,
+    width: 330,
+    paddingLeft: 16,
+    fontSize: 16,
+    lineHeight: 19,
+  },
+
+  pageHeader: {
+    flexDirection: "row",
+    backgroundColor: "#F2F2F2",
+    borderRadius: 40,
+    borderColor: "#0D0D0D",
+    borderStyle: "solid",
+    borderWidth: 3,
+    height: 57,
+    width: 330,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+
+  pageHeaderText: {
+    fontFamily: "Courier Prime Bold",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: 24,
+    lineHeight: 40,
+    color: "#358C7C",
+  },
+
+  safeView: {
+    flex: 1,
   },
 });
