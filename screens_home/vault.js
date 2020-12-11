9fe2cfa068f088a712ada939bd7b48e4865b369c
Josphze <43673761+Josphze@users.noreply.github.com>
2020-12-11 15:07:00 +0000

    Semi Vault

--- screens_home/vault.js
+++ screens_home/vault.js
@@ -1,59 +1,74 @@
 import React from "react";
-import { StatusBar, Text, View, StyleSheet } from "react-native";
+import {
+	ImageBackground,
+	StatusBar,
+	Text,
+	View,
+	StyleSheet,
+} from "react-native";
 import FormButton from "../components/FormButton";
-import AddAlbum from "../screens_home/addAlbum";
-import AddMusic from "../screens_home/addMusic";
 import VaultStack from "../navigation/vaultStack";
-global.vaultSwitch = 1;
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 class Vault extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <StatusBar backgroundColor="black" barStyle="light-content" />
-        <Text>Vault</Text>
-
-        <View style={styles.container}>
-          <View style={styles.button}>
-            <View style={styles.formButtons}>
-              <FormButton
-                onPress={() => {
-                  this.props.navigation.navigate("MyMusic");
-                }}
-                buttonTitle="My Music"
-              />
-              <FormButton
-                onPress={() => {
-                  this.props.navigation.navigate("MyAlbums");
-                }}
-                buttonTitle="My Albums"
-              />
-              <FormButton
-                onPress={() => {
-                  this.props.navigation.navigate("MyCollection");
-                }}
-                buttonTitle="My Collection"
-              />
-            </View>
-          </View>
-        </View>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<SafeAreaView style={styles.safeView}>
+				<StatusBar backgroundColor="black" barStyle="light-content" />
+				<ImageBackground
+					source={require("../assets/400x800.png")}
+					style={styles.backgroundImage}
+				>
+					<View style={styles.container}>
+						<View style={styles.formButtons}>
+							<FormButton
+								onPress={() => {
+									this.props.navigation.navigate("MyMusic");
+								}}
+								buttonTitle="My Music"
+							/>
+							<FormButton
+								onPress={() => {
+									this.props.navigation.navigate("MyAlbums");
+								}}
+								buttonTitle="My Albums"
+							/>
+							<FormButton
+								onPress={() => {
+									this.props.navigation.navigate(
+										"MyCollection"
+									);
+								}}
+								buttonTitle="My Collection"
+							/>
+						</View>
+					</View>
+				</ImageBackground>
+			</SafeAreaView>
+		);
+	}
 }
 
 export default Vault;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	backgroundImage: {
+		flex: 1,
+		resizeMode: "cover",
+	},
+
+	container: {
+		flex: 1,
+		alignItems: "center",
+		padding: 25,
+	},
+
+	safeView: {
+		flex: 1,
+	},
 });
