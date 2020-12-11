dd5143e0e6564096b5c4603a0f64dcc8bfe7e24b
Josphze <43673761+Josphze@users.noreply.github.com>
2020-12-11 14:59:40 +0000

    Dei copypasta duas vezes 🏇

--- screens_home/myCollection.js
+++ screens_home/myCollection.js
@@ -1,28 +1,56 @@
 import React from "react";
-import { StatusBar, Text, View, StyleSheet } from "react-native";
+import {
+	FlatList,
+	ImageBackground,
+	StatusBar,
+	StyleSheet,
+	Text,
+	View,
+} from "react-native";
+import FormButton from "../components/FormButton";
+import HeaderBar from "../components/HeaderBar";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 class MyCollection extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <StatusBar backgroundColor="black" barStyle="light-content" />
-        <Text>My Collection</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<SafeAreaView style={styles.safeView}>
+				<StatusBar backgroundColor="black" barStyle="light-content" />
+				<ImageBackground
+					source={require("../assets/400x800.png")}
+					style={styles.backgroundImage}
+				>
+					<HeaderBar
+						backgroundColor="#0D0D0D"
+						title="My Collection"
+						screenProps={this.props}
+						addOnPress={null}
+					/>
+					<View style={styles.container}></View>
+				</ImageBackground>
+			</SafeAreaView>
+		);
+	}
 }
 
 export default MyCollection;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	backgroundImage: {
+		flex: 1,
+		resizeMode: "cover",
+	},
+
+	container: {
+		flex: 1,
+		alignItems: "center",
+	},
+
+	safeView: {
+		flex: 1,
+	},
 });
