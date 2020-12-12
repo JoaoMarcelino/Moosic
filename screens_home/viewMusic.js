058b0670553cda28cc455145e22e9625e0b647f7
Josphze <43673761+Josphze@users.noreply.github.com>
2020-12-12 20:32:23 +0000

    Semi css no viewmusic

--- screens_home/viewMusic.js
+++ screens_home/viewMusic.js
@@ -1,11 +1,11 @@
 import React from "react";
 import {
-  FlatList,
-  ImageBackground,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
+	FlatList,
+	ImageBackground,
+	StatusBar,
+	StyleSheet,
+	Text,
+	View,
 } from "react-native";
 import FormButton from "../components/FormButton";
 import HeaderBar from "../components/HeaderBar";
@@ -13,23 +13,54 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { functions } from "firebase";
 
 const ViewMusic = (nav) => {
-  let item = nav.route.params.item;
-  return (
-    <View style={styles.container}>
-      <Text>Title: {item.title}</Text>
-      <Text>Artist: {item.artist}</Text>
-      <Text>Album: {item.album}</Text>
-      <Text>Listened: {"" + item.listened}</Text>
-    </View>
-  );
+	let item = nav.route.params.item;
+
+	return (
+		<SafeAreaView style={styles.safeView}>
+			<StatusBar backgroundColor="black" barStyle="light-content" />
+			<ImageBackground
+				source={require("../assets/400x800.png")}
+				style={styles.backgroundImage}
+			>
+				<View style={styles.container}>
+					<Text style={styles.text}>Title: {item.title}</Text>
+					<Text style={styles.text}>Artist: {item.artist}</Text>
+					<Text style={styles.text}>Album: {item.album}</Text>
+					<Text style={styles.text}>
+						Listened: {"" + item.listened}
+					</Text>
+				</View>
+			</ImageBackground>
+		</SafeAreaView>
+	);
 };
 
 export default ViewMusic;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	backgroundImage: {
+		flex: 1,
+		resizeMode: "cover",
+	},
+
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+
+	text: {
+		color: "#F2F2F2",
+	},
+
+	safeView: {
+		flex: 1,
+	},
 });
+
+/*<HeaderBar
+					backgroundColor="#0D0D0D"
+					title={item.title}
+					screenProps={this.props}
+					addOnPress={null}
+				/>*/
